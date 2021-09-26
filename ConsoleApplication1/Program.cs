cf89fd393f846c91cce14bab567b4ebe3b88b26c
Danyl <pochtaformumumu@gmail.com>
2021-09-26 13:22:30 +0300

    do stuff

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Play Game!");
+            BattleField battleField=new BattleField(10, 10);
+            battleField.Render();
+            battleField.AddShip(2, 0,0,0,1);
+            battleField.Hit(0,0);
+            battleField.Render();
+            battleField.Hit(0,1);
+            battleField.Render();
+            battleField.Hit(1,0);
+            battleField.Render();
+        }
+    }
+}
